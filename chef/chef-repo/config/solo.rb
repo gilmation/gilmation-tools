302c451439ee0a61f568861724202b5c6357bccd
Hugh Gilmour <shugster13@yahoo.es>
2009-09-30 16:37:10 +0200

    Created the gilmation_ee site-cookbook and updated some script.  Tested and running

--- chef/chef-repo/config/solo.rb
+++ chef/chef-repo/config/solo.rb
@@ -1,7 +1,7 @@
-file_store_path "/tmp/chef-solo"
-file_cache_path "/tmp/chef-solo"
-cookbook_path "/tmp/chef-solo/cookbooks"
-log_level :info
-log_location "/tmp/chef-solo/logs"
+file_store_path "/etc/chef"
+file_cache_path "/etc/chef"
+cookbook_path ["/etc/chef/cookbooks", "/etc/chef/site-cookbooks"]
+log_level :debug
+log_location "/etc/chef/log"
 ssl_verify_mode :verify_none
 Chef::Log::Formatter.show_time = true
---
+++ chef/chef-repo/site-cookbooks/gilmation_ee/recipes/default.rb
@@ -0,0 +1,53 @@
+#
+# Cookbook Name:: gilmation_ee
+# Recipe:: default
+#
+# Copyright 2009, Gilmation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "apache2"
+include_recipe "php::php5"
+include_recipe "mysql"
+
+# Now do the Gilmation specific stuff
+
+# Create the deployment root directory
+# Default action is create
+directories = [
+  "#{node[:gilmation][:webroot]}",
+  "#{node[:gilmation][:webroot]}/shared",
+  "#{node[:gilmation][:webroot]}/shared/assets",
+  "#{node[:gilmation][:webroot]}/shared/config",
+  "#{node[:gilmation][:webroot]}/shared/system",
+  "#{node[:gilmation][:webroot]}/releases" 
+]
+
+directories.each do |dir|
+  directory dir do
+    owner node[:gilmation][:deploy_user]
+    group node[:gilmation][:deploy_group]
+    mode node[:gilmation][:deploy_mode]
+    recursive true
+  end
+end
+
+# Add gilmation as a virtual host in Apache
+template "/etc/gilmation/apache2.conf" do
+  source "apache2.conf.erb"
+end
+
+apache_site "gilmation" do
+  config_path "/etc/gilmation/apache2.conf"
+end
