de069c453930590ab21d1f8519d36fce77ff69cf
Hugh Gilmour <hugh@gilmation.com>
2011-05-23 22:45:34 +0200

    Delete the ip webapp - moving it to it's own github project.

--- webapps/ip/config.ru
+++
@@ -1,3 +0,0 @@
-require './ip'
-run Ip
-
--- webapps/ip/ip_client.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-
-#
-# Get the Ip from a remote service
-#
-open('http://localhost:4567', :http_basic_authentication=>['admin', 'secret']) do |f|
-  puts f.gets
-end
-
